f84ce611e5005c27c07c2b5b7f0fe04d0f57fc03
Mostafa Elnaggar <90463112+MGElnaggar@users.noreply.github.com>
2024-02-12 22:33:29 +0200

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,9 +4,8 @@ import classes from './Home.module.css';
 const Home = () => {
 
     const handleAboutClick = () => {
-        // Scroll to the 650 horizontally of the page when the component is mounted
         window.scrollTo({
-            top: 606,
+            top: 605,
             left: 0,
             behavior: 'smooth'
         });
@@ -17,7 +16,6 @@ const Home = () => {
         <div className={classes.home}>
             <div className={classes.homecontent}>
                 <div className={classes.homecontentinfo}>
-                    {/* <HomeContent/> */}
                     <h5>HELLO, WORLD.</h5>
                     <h1>I'm Mostafa Elnaggar.</h1>
                     <p>
